42f588a5237a57b0e0f6cd80d2c43151fa99ce46
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-12-13 09:32:47 -0300

    Aula 03 - Finalização da tela de detalhes e leitura de QRCode

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
-import { View, Alert } from "react-native";
-
+import { View, Alert, Text } from "react-native";
+import MapView, { Callout, Marker } from 'react-native-maps';
+import { router } from "expo-router";
 import { api } from "@/services/api";
 
 import { Places } from "@/components/places";
 import { PlaceProps } from "@/components/place";
 import { Categories, CategoriesProps } from "@/components/categories";
+import { colors } from "@/styles/colors";
+import { fontFamily } from "@/styles/font-family";
 
-type MarketsProps = PlaceProps
+type MarketsProps = PlaceProps & {
+  latitude: number,
+  longitude: number,
+};
+
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+};
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([])
@@ -23,7 +36,7 @@ export default function Home() {
       console.log(error)
       Alert.alert("Categorias", "Não foi possível carregar as categorias.")
     }
-  }
+  };
 
   async function fetchMarkets() {
     try {
@@ -37,15 +50,15 @@ export default function Home() {
       console.log(error)
       Alert.alert("Locais", "Não foi possível carregar os locais.")
     }
-  }
+  };
 
   useEffect(() => {
-    fetchCategories()
-  }, [])
+    fetchCategories();
+  }, []);
 
   useEffect(() => {
     fetchMarkets()
-  }, [category])
+  }, [category]);
 
   return (
     <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
@@ -54,6 +67,56 @@ export default function Home() {
         onSelect={setCategory}
         selected={category}
       />
+
+      <MapView
+        style={{ flex: 1}}
+        initialRegion={currentLocation}
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude
+          }}
+          image={require("@/assets/location.png")}
+        />
+
+        {markets.map((item) => (
+          <Marker
+            key={item.id}
+            identifier={item.id}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude
+            }}
+            image={require("@/assets/pin.png")}
+          >
+            <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
+            <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.medium,
+                  }}
+                >
+                  {item.name}
+                </Text>
+
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.regular,
+                  }}
+                >
+                  {item.address}
+                </Text>
+              </View>
+            </Callout>
+          </Marker>
+         ))}
+      </MapView>
 
       <Places data={markets} />
     </View>
